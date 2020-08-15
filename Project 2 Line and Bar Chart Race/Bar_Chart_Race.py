00a1c79c297611e7a03710f368de6ef15567635c
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2020-08-15 22:02:25 +0545

    Add files via upload

---
+++ Project 2 Line and Bar Chart Race/Bar_Chart_Race.py
@@ -0,0 +1,58 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from IPython.display import HTML
+import matplotlib.patches as mpatches
+
+
+df = pd.read_excel("C:/Users/rosha/Desktop/demoo.xlsx")     
+df = df.set_index("Date")
+
+def prepare_data(df, steps=5):
+    df = df.reset_index()
+    df.index = df.index * steps
+    last_idx = df.index[-1] + 1
+    df_expanded = df.reindex(range(last_idx))
+    df_expanded['Date'] = df_expanded['Date'].fillna(method='ffill')
+    df_expanded = df_expanded.set_index('Date')
+    df_rank_expanded = df_expanded.rank(axis=1, method='first')
+    df_expanded = df_expanded.interpolate()
+    df_rank_expanded = df_rank_expanded.interpolate()
+    return df_expanded, df_rank_expanded
+
+df_expanded, df_rank_expanded = prepare_data(df)
+
+def nice_axes(ax):
+    #ax.set_facecolor('.8')
+    #ax.tick_params(labelsize=8, length=0)
+    #ax.grid(True, axis='x', color='black')
+    ax.set_axisbelow(True)
+    [spine.set_visible(False) for spine in ax.spines.values()]
+
+
+def init():
+    ax.clear()
+    nice_axes(ax)
+    ax.set_ylim(.2, 6.8)
+
+def update(i):
+    for bar in ax.containers:
+        bar.remove()
+    y = df_rank_expanded.iloc[i]
+    width = df_expanded.iloc[i]
+    ax.barh(y=y, width=width, color=plt.cm.Dark2(range(5)), tick_label=df_expanded.columns)
+    date_str = df_expanded.index[i].strftime('%B %d, %Y')
+    ax.set_title(f'Small Demo - {date_str}', fontsize='larger')
+    
+    
+
+    
+fig = plt.Figure(figsize=(7, 3), dpi=144)
+ax = fig.add_subplot()
+anim = FuncAnimation(fig=fig, func=update, init_func=init, frames=len(df_expanded), 
+                     interval=100, repeat=False)
+
+html = anim.to_html5_video()
+HTML(html)
+
+anim.save('C:/Users/rosha/Desktop/demoo.mp4')
