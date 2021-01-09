39ff021fd32c7031759b8f0347aa747765e91546
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-01-09 21:18:13 +0530

    Update append_excel_file.py

--- append_excel_file.py
+++ append_excel_file.py
@@ -1,15 +1,8 @@
 import pandas as pd
 
-a = []
-b = []
-
-for i in range(1,4):
-    df = pd.read_excel("C:/Users/Roshan Ghimire/Desktop/data" + str(i) +".xlsx")
-    for j in df['number'].values.tolist():
-        a.append(j)
-    for k in df['letter'].values.tolist():
-        b.append(k)
-    
-df = pd.DataFrame({"Number":a,
-                   "letter":b})
-df.to_excel("C:/Users/Roshan Ghimire/Desktop/final.xlsx")
+l = []
+for i in range(1,156):
+    df ="C:/Users/Roshan Ghimire/Desktop/Phase/Household" + str(i) +".csv"
+    l.append(df)
+a = pd.concat((pd.read_csv(file).assign(filename = file) for file in l),ignore_index=True)
+a.to_excel("C:/Users/Roshan Ghimire/Desktop/Final.xlsx")
