1fd0b505e3ae4dedef9602c1dd4e5d53661cb732
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-01-19 20:22:19 +0530

    Update append_excel_file.py

--- append_excel_file.py
+++ append_excel_file.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 l = []
-for i in range(1,156):
+for i in range(1,157):
     df ="C:/Users/Roshan Ghimire/Desktop/Phase/Household" + str(i) +".csv"
     l.append(df)
 a = pd.concat((pd.read_csv(file).assign(filename = file) for file in l),ignore_index=True)
