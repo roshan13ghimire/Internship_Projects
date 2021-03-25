28c5862140bc236b33be0eb1613f7569f5cc132e
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-03-25 09:43:50 +0530

    Update append_excel_file.py

--- append_excel_file.py
+++ append_excel_file.py
@@ -5,4 +5,4 @@ for i in range(1,157):
     df ="C:/Users/Roshan Ghimire/Desktop/Phase/Household" + str(i) +".csv"
     l.append(df)
 a = pd.concat((pd.read_csv(file).assign(filename = file) for file in l),ignore_index=True)
-a.to_excel("C:/Users/Roshan Ghimire/Desktop/Final.xlsx")
+a.to_excel("C:/Users/Roshan Ghimire/Desktop/Finall.xlsx")
